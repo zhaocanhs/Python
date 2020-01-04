63584bf7d4213c7085cb8cd3dd87d294062ff427
zhaocanhs <46547983+zhaocanhs@users.noreply.github.com>
2020-01-04 22:51:13 +0800

    基础学习

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,23 +1,98 @@
 #-*- coding:utf-8 -*-
 #作者：火小森
-print("hello world")
+import getpass  #密码模块
 
-print(7+3)
-name = "你好，世界"
-print(name)
+#输入输出-go
 
-'''注释多行或者打印多行'''
+# print("hello world")
+# # python 中数据类型不需要定义，系统会自动定义
+# print(7+3)
+# name = "你好，世界"
+# print(name)
+#
+# '''注释多行或者打印多行'''
+#
+# # 输入
+# name=input("name:")
+# age=int(input("age:")) #int()强制转换
+# # 打印数据类型
+# print(type(age))
+# job = input("job:")
+# salary=input("salary:")
+# # %s占位符
+# info='''-----info1 of   %s-------
+# Name:%s
+# Age:%d
+# Job:%s
+# Salary:%s
+# '''% (name, name,age,job,salary)
+# print(info)
+#
+# info2 = '''-----info2 of {_name}-------
+# Name:{_name}
+# Age:{_age}
+# Job:{_job}
+# Salary:{_salary}
+# '''.format(_name=name,
+#            _age=age,
+#            _job=job,
+#            _salary=salary)
+# print(info2)
+#
+# info3 = '''-----info3 of {0}-------
+# Name:{0}
+# Age:{1}
+# Job:{2}
+# Salary:{3}
+# '''.format(name,age,job,salary)
+# print(info3)
 
-# 输入
-name=input("name:")
-Age=input("age:")
-job=input("job:")
-Salary=input("salary:")
-# %s占位符
-info='''-----info of   %s-------
-Name:%s
-Age:%s
-Job:%s
-Salary:%s
-'''% (name,name,Age,job,Salary)
-print(info)
+#输入输出-end
+
+# if-else -go
+
+# 密码密封
+# _username="a"
+# _password="1"
+# username=input("username:")
+# # password=getpass.getpass("password:")
+# password=input("password:")
+#
+# if _username==username and  _password==password:
+#     print("Welcome user {name} login...".format(name=username))
+# else:
+#     print("Invalid username or password!")
+
+# if的嵌套
+# if:
+# elif:
+# else:
+
+# if-else -end
+
+# while  -go
+count=0
+while True:
+    print("count:",count)
+    count+=1
+
+# while可以和else结合；while的条件成立，执行while的循环体，否在不成立就执行else循环体。
+
+# while -end
+
+# for -go
+# for i in range(10):
+# else
+#     循环体
+#for -end
+
+# for -go
+
+#for -end
+
+# for -go
+#for -end
+
+# for -go
+
+#for -end
